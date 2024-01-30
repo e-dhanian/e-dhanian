096b39744e400c12c08874a05964324490e66cf1
Dhanian Ezra Opore <117781898+e-dhanian@users.noreply.github.com>
2024-01-31 01:33:01 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,1 +1,16 @@
-index.html
+// Created by Dhanian
+
+
+
+
+addEventListener("DOMContentLoaded",()=>{
+    
+});
+
+function openNav(){
+  document.querySelector(".sidenav").style.width="75%"
+}
+function closeNav(){
+  document.querySelector(".sidenav").style.width="0%"
+
+}
